597b326659b866c69f8504890a7d068d9fbdca0f
Clayton Salvador <clayton_rh@yahoo.com.br>
2022-10-21 15:09:20 -0300

    first commit

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+module exerciciosfx {
+	
+	
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;    
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;    
+    
+	opens basico;	
+}
